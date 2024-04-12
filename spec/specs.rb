7e6b2803782e2bb3a3c7fe6640bcb53305ba22b9
Scott Werner <stwerner@vt.edu>
2024-04-12 14:44:35 -0400

    update require statements and add RSpec.describe container block

--- spec/specs.rb
+++ spec/specs.rb
@@ -1,34 +1,37 @@
-require "../lib/solution.rb"
+require "rspec"
+require_relative "../lib/solution.rb"
 
-describe "box" do
-  before(:each) do
-    @box = Box.new
-    @box.add("red pen")
-  end
+RSpec.describe "box" do
+  describe "box" do
+    before(:each) do
+      @box = Box.new
+      @box.add("red pen")
+    end
 
-  it "knows what has been added" do
-    @box = Box.new
-    @box.add("red pen")
+    it "knows what has been added" do
+      @box = Box.new
+      @box.add("red pen")
 
-    expect(@box.has("red pen")).to be true
-  end
+      expect(@box.has("red pen")).to be true
+    end
 
-  it "can remove something it has" do
-    @box = Box.new
-    @box.add("red pen")
+    it "can remove something it has" do
+      @box = Box.new
+      @box.add("red pen")
 
-    @box.remove("red pen")
-    expect(@box.has("red pen")).to be false
-  end
+      @box.remove("red pen")
+      expect(@box.has("red pen")).to be false
+    end
 
-  it "knows what it doesn't have" do
-    @box = Box.new
-    @box.add("red pen")
+    it "knows what it doesn't have" do
+      @box = Box.new
+      @box.add("red pen")
 
-    expect(@box.has("purple smartphone")).to be false
-  end
+      expect(@box.has("purple smartphone")).to be false
+    end
 
-  it "knows how many items have been added - when only one" do
-    expect(@box.number_of_items).to eq(1)
+    it "knows how many items have been added - when only one" do
+      expect(@box.number_of_items).to eq(1)
+    end
   end
 end
