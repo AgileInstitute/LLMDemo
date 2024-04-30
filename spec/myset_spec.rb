e15f1aec19897bdff00755ed8a1d09fd801b6113
Rob Myers <Rob.Myers@agileInstitute.com>
2024-04-30 15:10:09 -0700

    /Bob/ writes union function in one go!
    /Bob/ also decided we needed an untested and unrequested to_a ("to array") method, or maybe /Bob/ is fond of self-encapsulation?

--- spec/myset_spec.rb
+++ spec/myset_spec.rb
@@ -4,6 +4,18 @@ describe "myset" do
   before(:each) do
     array_of_elements = ["red pen"]
     @myset = MySet.new(array_of_elements)
+
+    @set_axb = MySet.new(["a", "x", "b"])
+    @set_xyz = MySet.new(["x", "y", "z"])
+  end
+
+  it "can build a union from two other sets" do
+      union = @set_axb.union(@set_xyz)
+      expect(union.has("a")).to be true
+      expect(union.has("b")).to be true
+      expect(union.has("x")).to be true
+      expect(union.has("y")).to be true
+      expect(union.has("z")).to be true
   end
 
   it "knows it is empty" do
