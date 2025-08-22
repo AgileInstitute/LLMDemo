68bd7cc38199a74c9e3a7b478cdf07363b2ccac2
Rob Myers <Rob.Myers@agileInstitute.com>
2025-08-22 16:42:44 -0700

    Claude wrote three error tests. /Bob/ wrote the code!

--- example.rb
+++ example.rb
@@ -4,14 +4,14 @@ require './rspec_source_code_generator'
 
 # copies the previous implementation code (i.e., 'bot output) to a backup
 # for quick comparison (not totally necessary since we have a git repo...)
-FileUtils.cp("./lib/nostrek.rb", "./lib/PREVIOUS_nostrek.rb")
+FileUtils.cp("./lib/salvo.rb", "./lib/PREVIOUS_salvo.rb")
 
 # reads the whole specifications file as a string
-specsFile = File.open("./spec/nostrek_spec.rb")
+specsFile = File.open("./spec/salvo_spec.rb")
 theTests = specsFile.read
 specsFile.close
 
 # overwrites the implementation file with the newly generated code
 generator = RspecSourceCodeGenerator.new(unit_tests:theTests)
 solution = generator.generate
-File.write("./lib/nostrek.rb", solution)
+File.write("./lib/salvo.rb", solution)
---
+++ lib/salvo.rb
@@ -0,0 +1,24 @@
+class Board
+  def initialize
+    @grid = Array.new(10) { Array.new(10) }
+  end
+
+  def place(ship, x, y)
+    raise 'Invalid board position!' if x < 0 || y < 0 || x >= 10 || y >= 10
+
+    @grid[x][y] = ship
+  end
+
+  def whats_at(x, y)
+    @grid[x][y]
+  end
+end
+
+class Ship
+  def initialize(id)
+    @id = id
+  end
+end
+
+# Note: The commented positions (0,10), (10,9), and (9,-1) in the tests suggest
+# that the board dimensions are from 0 to 9 inclusive, any position outside of this raises an error.
