cf78defff8b7cd340549c5da9bcaeb3e5877f1f9
Rob Myers <Rob.Myers@agileInstitute.com>
2025-08-22 16:45:13 -0700

    Refactor tests
    Claude refactored the tests for me, reducing duplication.

--- spec/salvo_spec.rb
+++ spec/salvo_spec.rb
@@ -8,28 +8,29 @@ describe 'when placing ships on boards' do
     @ship = Ship.new(1)
   end
 
+  def expect_invalid_position(row, column)
+    expect { @board.place(@ship, row, column) }
+      .to raise_error('Invalid board position!')
+  end
+
   it 'will place a ship where asked' do
     @board.place(@ship, 0, 0)
     expect(@board.whats_at(0, 0)).to eq(@ship)
   end
 
   it 'will NOT place a ship off the board when negative row' do
-    expect { @board.place(@ship, -1, 0) }
-      .to raise_error('Invalid board position!')
+    expect_invalid_position(-1, 0)
   end
 
   it 'will NOT place a ship when column out of bounds' do
-    expect { @board.place(@ship, 0, 10) }
-      .to raise_error('Invalid board position!')
+    expect_invalid_position(0, 10)
   end
 
   it 'will NOT place a ship when row out of bounds' do
-    expect { @board.place(@ship, 10, 9) }
-      .to raise_error('Invalid board position!')
+    expect_invalid_position(10, 9)
   end
 
   it 'will NOT place a ship when negative column' do
-    expect { @board.place(@ship, 9, -1) }
-      .to raise_error('Invalid board position!')
+    expect_invalid_position(9, -1)
   end
 end
