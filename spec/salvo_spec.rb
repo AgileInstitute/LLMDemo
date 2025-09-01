90567ce1c6ecb1dd3aad87617beaf2aedca08f77
Rob Myers <Rob.Myers@agileInstitute.com>
2025-09-01 15:13:44 -0700

    more test refactoring

--- spec/salvo_spec.rb
+++ spec/salvo_spec.rb
@@ -38,12 +38,12 @@ describe 'when placing ships on boards' do
     expect { @board.place(Ship.new(1), 0, 1) }.to raise_error('Ships should not be adjacent!')
   end
 
-  it 'will not place a ship if another one is already there' do
+  it 'will not place a ship if another one shares a starting position' do
     @board.place(@tiny_ship, 0, 0)
     expect { @board.place(Ship.new(1), 0, 0) }.to raise_error('There is already something there!')
   end
 
-  it 'with a bigger ship will not place a ship if another one is already there' do
+  it 'will not place a ship if another one is already there' do
     @board.place(@long_ship, 0, 0)
     expect { @board.place(Ship.new(1), 0, 2) }.to raise_error('There is already something there!')
   end
