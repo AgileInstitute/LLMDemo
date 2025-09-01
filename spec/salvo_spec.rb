b08841cdfcaded74481906800419f2703044f524
Rob Myers <Rob.Myers@agileInstitute.com>
2025-09-01 15:10:22 -0700

    some test refactoring

--- spec/salvo_spec.rb
+++ spec/salvo_spec.rb
@@ -5,22 +5,41 @@ require 'salvo'
 describe 'when placing ships on boards' do
   before(:each) do
     @board = Board.new
-    @ship = Ship.new(1)
+    @tiny_ship = Ship.new(1)
     @long_ship = Ship.new(3)
   end
 
-  def expect_invalid_position(row, column)
-    expect { @board.place(@ship, row, column, 'horizontal') }
-      .to raise_error('Ship begins beyond board!')
+  describe 'out of bounds positions raise an error' do
+    def expect_invalid_position(row, column)
+      expect { @board.place(@tiny_ship, row, column, 'horizontal') }
+        .to raise_error('Ship begins beyond board!')
+    end
+
+    it 'will NOT place a ship with negative row' do
+      expect_invalid_position(-1, 0)
+    end
+
+    it 'will NOT place a ship when column out of bounds' do
+      expect_invalid_position(0, 10)
+    end
+
+    it 'will NOT place a ship when row out of bounds' do
+      expect_invalid_position(10, 9)
+    end
+
+    it 'will NOT place a ship with negative column' do
+      expect_invalid_position(9, -1)
+    end
   end
 
+
   it 'will not place a ship adjacent to another one' do
-    @board.place(@ship, 0, 0)
+    @board.place(@tiny_ship, 0, 0)
     expect { @board.place(Ship.new(1), 0, 1) }.to raise_error('Ships should not be adjacent!')
   end
 
   it 'will not place a ship if another one is already there' do
-    @board.place(@ship, 0, 0)
+    @board.place(@tiny_ship, 0, 0)
     expect { @board.place(Ship.new(1), 0, 0) }.to raise_error('There is already something there!')
   end
 
@@ -30,13 +49,13 @@ describe 'when placing ships on boards' do
   end
 
   it 'will place a tiny ship at upper left with redundant orientation' do
-    @board.place(@ship, 0, 0, 'horizontal')
-    expect(@board.whats_at(0, 0)).to eq(@ship)
+    @board.place(@tiny_ship, 0, 0, 'horizontal')
+    expect(@board.whats_at(0, 0)).to eq(@tiny_ship)
   end
 
   it 'will place a tiny ship at lower right with redundant orientation' do
-    @board.place(@ship, 9, 9, 'vertical')
-    expect(@board.whats_at(9, 9)).to eq(@ship)
+    @board.place(@tiny_ship, 9, 9, 'vertical')
+    expect(@board.whats_at(9, 9)).to eq(@tiny_ship)
   end
 
   it 'will place a long ship horizontally' do
@@ -55,21 +74,6 @@ describe 'when placing ships on boards' do
     expect(@board.whats_at(3, 0)).to be_nil
   end
 
-  it 'will NOT place a ship off the board when negative row' do
-    expect_invalid_position(-1, 0)
-  end
-
-  it 'will NOT place a ship when column out of bounds' do
-    expect_invalid_position(0, 10)
-  end
-
-  it 'will NOT place a ship when row out of bounds' do
-    expect_invalid_position(10, 9)
-  end
-
-  it 'will NOT place a ship when negative column' do
-    expect_invalid_position(9, -1)
-  end
 
   it 'will NOT place a long ship horizontally if it extends beyond board' do
     expect { @board.place(@long_ship, 0, 8, 'horizontal') }
@@ -82,18 +86,18 @@ describe 'when placing ships on boards' do
   end
 
   it 'will NOT place a ship diagonally' do
-    expect { @board.place(@ship, 0, 0, 'diagonal') }
+    expect { @board.place(@tiny_ship, 0, 0, 'diagonal') }
       .to raise_error('Invalid orientation!')
   end
 
   it 'will NOT place a horizontal ship if it collides with existing ship' do
-    @board.place(@ship, 0, 1)
+    @board.place(@tiny_ship, 0, 1)
     expect { @board.place(@long_ship, 0, 0, 'horizontal') }
       .to raise_error('There is already something there!')
   end
 
   it 'will NOT place a vertical ship if it collides with existing ship' do
-    @board.place(@ship, 1, 0)
+    @board.place(@tiny_ship, 1, 0)
     expect { @board.place(@long_ship, 0, 0, 'vertical') }
       .to raise_error('There is already something there!')
   end
